986cfb4e80d7c537e96572522266f2aa75a2a624
liggiorgio <liggiorgio@live.com>
2019-06-04 19:40:25 +0200

    Implement sound manager

---
+++ Game/SundayKnights/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    The sound manager is in charge of playing sound effects depending on what
+    is happening during the game: mainly, it plays the sfx for tile matches.
+*/
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip matchAudioClip;
+    AudioSource match;
+
+    // Awake is called after creation
+    void Awake()
+    {
+        match = AddAudio(matchAudioClip);
+    }
+
+    AudioSource AddAudio(AudioClip audioClip)
+    {
+        AudioSource audioSource = this.gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.clip = audioClip;
+        return audioSource;
+    }
+
+    public void PlayMatch()
+    {
+        match.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
