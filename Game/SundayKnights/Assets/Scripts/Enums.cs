a1be4976d6a36bbf17566a08754c3a2ed416f0bd
liggiorgio <liggiorgio@live.com>
2019-06-04 18:08:59 +0200

    Add game enumerations

---
+++ Game/SundayKnights/Assets/Scripts/Enums.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+    Enumerations used to track tiles' special effects (plus utility routines),
+    and the game state when a game is running.
+*/
+
+[Flags]
+public enum BonusType
+{
+    None,
+    DestroyRowCol
+}
+
+public static class BonusTypeUtils
+{
+    // Return if bonus is the same
+    public static bool ContainsDestroyRowCol(BonusType bt)
+    {
+        return (bt & BonusType.DestroyRowCol) == BonusType.DestroyRowCol;
+    }
+}
+
+public enum GameState
+{
+    None,
+    Selecting,
+    Animating
+}
