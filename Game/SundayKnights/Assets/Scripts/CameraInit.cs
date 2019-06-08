f8b1cc4a77fe08995497f8ba4ff50fffaf25fdc7
liggiorgio <liggiorgio@live.com>
2019-06-08 02:36:26 +0200

    Fix camera and screen ratio across devices

---
+++ Game/SundayKnights/Assets/Scripts/CameraInit.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraInit : MonoBehaviour {
+
+	public float targetAspect;
+
+	void Start ()
+	{
+		float windowAspect = (float) Screen.width / (float) Screen.height;
+		float scaleHeight = windowAspect / targetAspect;
+		Camera camera = GetComponent<Camera>();
+
+		if (scaleHeight < 1.0f)
+		{
+			camera.orthographicSize = camera.orthographicSize / scaleHeight;
+		}
+	}
+}
