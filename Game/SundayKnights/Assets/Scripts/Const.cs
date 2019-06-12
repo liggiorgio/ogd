b4d6793810e06d4c7440b094d7d56f254439a107
liggiorgio <liggiorgio@live.com>
2019-06-12 14:51:49 +0200

    Update grid size, animation duration

--- Game/SundayKnights/Assets/Scripts/Const.cs
+++ Game/SundayKnights/Assets/Scripts/Const.cs
@@ -5,12 +5,12 @@
 public class Const
 {
     // Game grid
-    public static readonly int Rows = 8;
+    public static readonly int Rows = 6;
     public static readonly int Columns = 6;
 
     // Animations
-    public static readonly float AnimationDuration = 0.1f;
-    public static readonly float MoveAnimationMinDuration = 0.1f;
+    public static readonly float AnimationDuration = 0.125f;
+    public static readonly float MoveAnimationMinDuration = 0.15f;
     public static readonly float ExplosionDuration = 0.3f;
     public static readonly float WaitBeforePotentialMatchesCheck = 2f;
     public static readonly float OpacityAnimationFrameDelay = 0.05f;
