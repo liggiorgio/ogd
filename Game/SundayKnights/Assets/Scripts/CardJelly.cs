bb1fdd2ae1b8d6cf8897709bf6b5b52926028a12
Alessandro Costella <alessandrocostella@hotmail.it>
2019-06-21 10:31:37 +0200

    Jelly Card makes pseudojelly appear

--- Game/SundayKnights/Assets/Scripts/CardJelly.cs
+++ Game/SundayKnights/Assets/Scripts/CardJelly.cs
@@ -6,15 +6,27 @@ public class CardJelly : MonoBehaviour
 {
     // Change backgound colour of tiles, make animation slower, remove after 3 matches in area (if easy to implement, combos count)
 
+	public GameObject jelly;
+	private TilesManager tilesManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tilesManager = GameObject.Find("TilesManager").GetComponent<TilesManager>();
+		jelly.active = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ( (tilesManager.state == GameState.None) || (tilesManager.state == GameState.Selecting) )
+        {
+            // This input will change once networking is up
+            if ( Input.GetMouseButton(0) )
+            {
+				jelly.active = true;
+				
+			}
+		}
     }
 }
